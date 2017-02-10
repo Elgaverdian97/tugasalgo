8e79ac44bbec4feb9f3a3c51919d30d3573a2c2a
Elgaverdian97 <eglaverdian@gmail.com>
2017-02-10 17:37:27 +0700

    Update latihan5.cpp

--- latihan5.cpp
+++ latihan5.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 	int main (){
 		typedef int EGLA;
-		cout << "ukuran char:" << sizeof (char)<<endl;   			// menampilkan ukuran type data char
-		cout << "ukuran int :" << sizeof (EGLA) << endl;			// menampilkan ukuran type data Integer
-		cout << "ukuran Short Int : " << sizeof(short int) << endl; // menampilkan ukuran type data shor int
+		cout << "ukuran char:" << sizeof (char)<<endl;                  // menampilkan ukuran type data char
+		cout << "ukuran int :" << sizeof (EGLA) << endl;		// menampilkan ukuran type data Integer
+		cout << "ukuran Short Int : " << sizeof(short int) << endl;     // menampilkan ukuran type data shor int
 		cout << "ukuran Long Int : " << sizeof(long int) << endl; 	// menampilkan ukuran type data long int
 		cout << "ukuran Float : " << sizeof(float) << endl; 		// menampilkan ukuran type data float
 		cout << "ukuran Double : " << sizeof(double) << endl; 		// menampilkan ukuran type data double
