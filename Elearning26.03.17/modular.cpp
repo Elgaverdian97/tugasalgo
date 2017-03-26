ea60ba04dc2f0dd39d8c8dfffc1192ff36d4633d
Elgaverdian97 <eglaverdian@gmail.com>
2017-03-26 12:43:00 +0700

    Add files via upload

---
+++ Elearning26.03.17/modular.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+// {i.s} Inisialisasi Fungsi
+// {f.s} Pemanggilan Fungsi
+using namespace std;
+
+// Inisialisasi Fungsi
+ void sample () {
+	cout << "================================" <<endl;
+	cout << "===> WELCOME TO MY PROGRAM <===" << endl;
+	cout << endl;
+}
+
+//Inisialisai Fungsi
+ void Nama(string nama) {
+	 
+  cout << "    HELLO:"<< nama << endl;
+}
+
+int main(){
+
+  sample(); // pemanggilan fungsi
+
+
+  Nama("ELGA VERDIAN MARTIN"); // pemanggilan fungsi
+  
+
+  cout << "================================" <<endl;
+
+  return 0;
+}
